9665db1fa22f832159d82978cccaaf7ca1c9497e
gauravthapar <gauravthapar34@gmail.com>
2020-06-18 01:43:30 +0530

    linear search

---
+++ LinearSearch.py
@@ -0,0 +1,22 @@
+def linearsearch(lst, key):
+    for position in range(0,len(lst)) :
+        if lst[position] == key :
+            return position
+            break
+        else:
+            continue
+    
+
+lst = []
+lenght = int(input('Enter the no. of items :'))
+for i in range(0,lenght):
+    element = int(input())
+    lst.append(element)
+
+key = int(input('Enter the item you want to search :'))
+
+result = linearsearch(lst,key)
+if result == None:
+    print('Item Not Found')
+else:
+    print('Item Found at index :',result)
